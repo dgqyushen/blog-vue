7b00766ab4427eae86e3b1d8bf86d024cd2dcf71
dgqyushen <2446682260@qq.com>
2021-07-09 23:42:39 +0800

    fix bugs

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,9 +10,14 @@
   <v-footer color="#2E5879" padless height="136" style="margin-top: 30px">
   <v-row justify="center" no-gutters>
 <!--    <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">{{ link }}</v-btn>-->
-    <v-col class=" lighten-2 py-4 text-center white--text" cols="12">
+    <v-col class="lighten-2 py-4 text-center white--text" cols="12">
       <strong>{{ new Date().getFullYear() }} </strong>— <strong>Powered By dgqyushen</strong>
+      <div>
+        <strong><a href="https://beian.miit.gov.cn/" style="color: white">粤ICP备2021088567号-1</a> | <img src="../assets/img/beian.png" alt="备案图标" style="height: 15px;"><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44190002005636" style="color: white"> 粤公网安备44190002005636号</a></strong>
+      </div>
     </v-col>
+
+
   </v-row>
   </v-footer>
 </template>
